3986c5cbcff6bfbb4d0ebb10b7291a571c0ee7e2
Usman <91799557+DirtyPaki1@users.noreply.github.com>
2024-03-19 14:38:35 -0400

    Completed

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -4,3 +4,10 @@ import '@testing-library/jest-dom';
 import App from "../App";
 
 // Your tests here
+test("displays a top-level heading with the text `Hi, I'm _______`", () => {
+    // Arrange
+    render(<App />);
+  
+    // Act
+})
+  
